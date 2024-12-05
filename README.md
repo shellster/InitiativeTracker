d483d96f0f09626f7110901f64b9f3968d41cb36
shellster <zn8v6c3f5nw9@blurmail.net>
2024-12-05 14:41:56 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# TTRPG Initiative Tracker
+
+This project allows TTRPG initative tracking via small screens typically used for displaying PC Hardware settings
+
+Before attempting to use this library please carefully read the "Acknowledgements and Important Compatibility Notes" section.
+
+
+## Instructions
+
+### Install Requirements
+
+Install python3.10+.
+
+Use your preferred virtual environment creation method and create a virtual environment in the root project folder and enter enter the virtual environment.  If you are not familar with python, I recommend `pipenv`.  After installing it you can simply run:
+
+`pipenv shell` 
+
+Install the required libraries:
+
+`pip install -r requirements.txt`
+
+
+### Configure Settings
+
+The initative tracker is operated from a simple `initative.json` config file (by default, or you can provide a path as an argument when you run the tracker).  A sample is provided in this repo, and
+we will go through it line by line here, for the configurable options.
+
+**IMPORTANT NOTE ON COLORS**:
+Several of the options deal with color specifications.  Each color should be specified as an array of three integers from 0-255, in the order: [red, green, blue].  
+On my RevA, I've found that some perfectly valid color combinations will cause to screen to glitch out and lock-up, requiring a power cycle.  If you experience this, try 
+tweaking your color values slightly until you find something that works.
+
+* **screen_port**: Defaults to `"AUTO"`.  This corresponds to the Serial/COM port the screen you are using is attached to.  In most cases you can
+either not specify it or set it to `"AUTO"` for autodetection of the appropriate port.
+
+* **screen_model**: Defaults to `"LcdCommRevA"`. This corresponds to a supported model of screen.  Current supported options are: `"LcdCommRevA"`, `"LcdCommRevB"`, `"LcdCommRevC"`, `"LcdCommRevD"`, `"LcdSimulated"`
+
+* **screen_brightness_percentage**: Defaults to `100`.  This is an integer representing the percentage brightness the screen should be.  Apparently original Turing RevA screens warn about getting hot at 100%, but I have not observed any problems.
+
+* **default_foreground_color**: Defaults to `[0, 9, 148]`.  This is the default font color if no other special condition applies.
+
+* **default_background_color**: Defaults to `[0, 0, 0]`.  This is the default background color for the row if no other special condition applies.
+
+* **currently_up_foreground_color**: Defaults to `[0, 9, 148]`.  This is the font color of the player(s) currently acting.
+
+* **currently_up_background_color**: Defaults to `[255, 255, 255]`.  This is the background color for the row(s) of the player(s) currently acting.
+
+* **on_deck_foreground_color**: Defaults to `[0, 9, 148]`.  This is the font color of the player(s) next to act.
+
+* **on_deck_background_color**: Defaults to `[229, 194, 157]`.  This is the background color for the row(s) of the player(s) next to act.
+
+* **enemy_foreground_color**: If provided, enemy font color will always appear as this in the initiative list, otherwise enemy will be the same colors as a player for currently up, on deck, other.
+
+* **enemy_background_color**: If provided, enemy background row color will always appear as this in the initiative list, otherwise it will be the same background colors as a player for currently up, on deck, other.
+
+* **player_order**: This is object where the keys are the player's names (or player-aligned characters in this initiative) and the values are either an integer representing their initiative order, or an array of two values 
+representing the inclusive range of their initative.  In the latter case, a random initiative value in the provided range will be rolled at runtime.  Order of players in object is not imporant (they are sorted at runtime).  
+Adding a `-` to the front of the player name will result in the player not appearing in the initiative list (a quick way to temporarily remove a player without deleting them).  Player names will be truncated as necessary to 
+fit on the screen.  For mine code will truncate at 11 characters.
+
+* **enemy_order**: This follows all the same rules and configuration options as player_order, but it represents any "enemy" characters in relation to the players.
+
+### Run Tracker
+
+Once you have configured your initiative.json file, you can run the program like so:
+
+python3 tracker.py `path/to/initiative.json`  (path is optional, will default to `initiative.json`)
+
+Assuming you've configured everything correctly, the initiative should appear after a few seconds on your little screen.
+
+In your console, you'll see something like this:
+
+```
+Currently Up:
+        Bob
+
+On Deck:
+        Korak
+        Hobgoblin 1
+
+Next?
+```
+
+This allows the DM to see the currently active entity and upcoming entities while having the screen pointing out to the players.
+
+The "Next?" message will only appear once the screen is fully drawn (which can take several seconds).
+
+Pressing `Enter` will advance the initiative one entity.
+
+`Ctrl + C` will turn off the screen and exit.
+
+**Note on Initiative Order**: If a player or players and one or more enemies share initiative order, the code will always ensure that the player(s) appear first in the initiative list.
+
+
+## Acknowledgements and Important Compatibility Notes
+
+This project absolutely would not be possible without the fantastic work and library found here:
+https://github.com/mathoudebine/turing-smart-screen-python
+
+Support for screens is thus reliant on the screen support provided by this library.  If you wish to utilize this module, please ensure
+that your screen is compatible or alternatively try the LcdSimulated option.  Furthermore, I have only tested the configuration on an 
+LcdCommRevA.  For other screens, you may need to adjust the font sizes, ClearScreen function, and other parameters if the existing ones 
+don't work well (they almost certainly will not for other resolution screens)
+
+Pull Requests are welcome and appreciated for increasing support.
+
+If additional screen revisions become available, providing the code does not otherwise change, you should be able to copy the new revision
+from turing-smart-screen-python.library.lcd.<revision> into this projects library.lcd folder.  Then add it to the list of models in
+`get_lcd_instance`
---
+++ tracker.py
@@ -0,0 +1,183 @@
+import importlib
+import json
+import logging
+import secrets
+import sys
+from unittest import mock
+
+# Ugly hack to prevent log file from getting dropped
+sys.modules["library.log"] = mock.Mock()
+sys.modules["library.log.logger"] = logging.getLogger()
+
+from library.lcd.lcd_comm import Orientation
+
+MAX_NAMES_SHOW = 5
+LEFT_PAD = 5
+FONT_SIZE = 95
+VERTICAL_SIZE = 90
+MAX_CHAR_WIDTH = 14
+FONT = "roboto-mono/RobotoMono-SemiBold.ttf"
+
+SCREEN_PORT = "AUTO"
+SCREEN_MODEL = "LcdCommRevA"
+SCREEN_BRIGHTNESS_PERCENTAGE = 100
+CURRENTLY_UP_FOREGROUND_COLOR = (0, 9, 148)
+CURRENTLY_UP_BACKGROUND_COLOR = (255, 255, 255)
+DEFAULT_FOREGROUND_COLOR = (0, 9, 148)
+DEFAULT_BACKGROUND_COLOR = (0, 0, 0)
+ON_DECK_FOREGROUND_COLOR = (0, 9, 148)
+ON_DECK_BACKGROUND_COLOR = (229, 194, 157)
+
+
+def get_lcd_instance(model_name, port):
+    """Dynamically load appropriate library for screen model"""
+
+    default_display_config = {
+        "max_names_show": 5,
+        "left_pad": 5,
+        "font": "roboto-mono/RobotoMono-SemiBold.ttf",
+        "font_size": 95,
+        "vertical_size": 90,
+        "max_char_width": 14,
+    }
+
+    """
+    For some reason the simulated font scaling is different than on the RevA.  The following settings
+    seem to relatively closely match the RevA results.
+    """
+
+    simulated_display_config = default_display_config.copy() | {
+        "font_size": 60,
+        "vertical_size": 50,
+        "max_names_show": 5,
+        "max_char_width": 12,
+    }
+
+    models = {
+        "LcdCommRevA": {"path": "library.lcd.lcd_comm_rev_a", "display_config": default_display_config},
+        "LcdCommRevB": {"path": "library.lcd.lcd_comm_rev_b", "display_config": default_display_config},
+        "LcdCommRevC": {"path": "library.lcd.lcd_comm_rev_c", "display_config": default_display_config},
+        "LcdCommRevD": {"path": "library.lcd.lcd_comm_rev_d", "display_config": default_display_config},
+        "LcdSimulated": {"path": "library.lcd.lcd_simulated", "display_config": simulated_display_config}
+    }
+
+    library = importlib.import_module(models[model_name]["path"])
+    BaseLCDClass = getattr(library, model_name)
+
+    class ClearScreenMixin:
+        def ClearScreen(self):
+            self.ScreenOff()
+            self.ScreenOn()
+
+    return type('LCD', (BaseLCDClass, ClearScreenMixin), {"display_config": models[model_name]["display_config"]})(com_port=port)
+
+
+def draw_screen(commands):
+    lcd_comm.ClearScreen()
+    for command in commands:
+        lcd_comm.DisplayText(**command)
+
+def load_config():
+    config_file = None
+
+    if len(sys.argv) == 1:
+        config_file = "initiative.json"
+    else:
+        if sys.argv[1] in ["-h", "--help"] or len(sys.argv) != 2:
+            print(f"Usage:\n\tpython {sys.argv[0]} <path/to/configuration.json>")
+            sys.exit(1)
+        else:
+            config_file = sys.argv[1]
+
+    with open(config_file) as f:
+        config = json.load(f)
+        
+        config.setdefault("screen_port", SCREEN_PORT)
+        config.setdefault("screen_model", SCREEN_MODEL)
+        config.setdefault("screen_brightness_percentage", SCREEN_BRIGHTNESS_PERCENTAGE)
+        config.setdefault("on_deck_foreground_color", ON_DECK_FOREGROUND_COLOR)
+        config.setdefault("on_deck_background_color", ON_DECK_BACKGROUND_COLOR)
+        config.setdefault("currently_up_foreground_color", CURRENTLY_UP_FOREGROUND_COLOR)
+        config.setdefault("currently_up_background_color", CURRENTLY_UP_BACKGROUND_COLOR)
+        config.setdefault("default_foreground_color", DEFAULT_FOREGROUND_COLOR)
+        config.setdefault("default_background_color", DEFAULT_BACKGROUND_COLOR)
+        config.setdefault("enemy_foreground_color", None)
+        config.setdefault("enemy_background_color", None)
+
+        return config
+
+
+if __name__ == "__main__":
+    config = load_config()
+
+    lcd_comm = get_lcd_instance(config["screen_model"], config["screen_port"])
+    lcd_comm.InitializeComm()
+    lcd_comm.SetOrientation(orientation=Orientation.LANDSCAPE)
+    lcd_comm.ClearScreen()
+    lcd_comm.SetBrightness(level=config["screen_brightness_percentage"])
+
+    initiative_order = config["player_order"].copy()
+    initiative_order.update(config["enemy_order"].copy())
+    initiative_order = {k:(v if isinstance(v, int) else secrets.choice(range(v[0], v[1] + 1))) for k,v in initiative_order.items() if not k.startswith("-")}
+
+    ordered = [k for k, _ in sorted(initiative_order.items(), key=lambda item: item[1] - (1 if item[0] in config["enemy_order"] else 0))]
+
+    try:
+        while len(initiative_order):
+            current_initiative = None
+            next_initative = None
+
+            current_up = []
+            next_up = []
+
+            staged_commands = []
+
+            for row, key in enumerate((ordered[::-1][0: min(lcd_comm.display_config["max_names_show"], len(ordered))])):
+                
+                if row == 0:
+                    current_initiative = initiative_order[key]
+
+                if current_initiative == initiative_order[key]:
+                    current_up.append(key)
+                elif next_initative is None:
+                    next_initative = initiative_order[key]
+            
+                if next_initative == initiative_order[key]:
+                    next_up.append(key)
+
+                background_color = config["default_background_color"]
+                foreground_color = config["default_foreground_color"]
+
+                if current_initiative == initiative_order[key]:
+                    background_color = config["currently_up_background_color"]
+                    foreground_color = config["currently_up_foreground_color"]
+                elif next_initative == initiative_order[key]:
+                    background_color = config["on_deck_background_color"]
+                    foreground_color = config["on_deck_foreground_color"]
+                
+                if key in config["enemy_order"]:
+                    background_color = config.get("enemy_background_color", background_color)
+                    foreground_color = config.get("enemy_foreground_color", foreground_color)
+
+                staged_commands.append({
+                    "text": f"{key[:lcd_comm.display_config["max_char_width"] - 3].ljust(lcd_comm.display_config["max_char_width"] - 3)} {str(initiative_order[key]).rjust(2)}", 
+                    "x": lcd_comm.display_config["left_pad"],
+                    "y": row * lcd_comm.display_config["vertical_size"], 
+                    "font": lcd_comm.display_config["font"],
+                    "font_size": lcd_comm.display_config["font_size"],
+                    "font_color": tuple(foreground_color),
+                    "background_color": tuple(background_color)
+                })          
+
+
+            print(f"Currently Up:\n\t{"\n\t".join(current_up)}\n\nOn Deck:\n\t{"\n\t".join(next_up)}\n\n")
+            draw_screen(staged_commands)
+            input("Next?")
+
+            for i in range(len(current_up)):
+                name = ordered.pop()
+                ordered.insert(0, name)
+
+    except KeyboardInterrupt:
+        lcd_comm.ScreenOff()
+        lcd_comm.closeSerial()
